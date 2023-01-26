e7a49c17303fd3415bc77d66ceaf0f615b829270
Sszyh <zhuyer@outlook.com>
2023-01-26 12:40:55 -0800

    add TestCard for test fetching data from backend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../styles/Home.css'
 import Banner from '../components/Banner'
+import TestCard from './TestCard'
 
 function Home() {
   return (
     <div className='home'>
       <Banner />
+      <TestCard />
     </div>
   )
 }
---
+++ src/components/TestCard.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import '../styles/Header.css'
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Fragment } from 'react';
+
+export default function TestCard() {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    
+    fetch("http://localhost:8000/properties")
+
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data,"dataaa")
+        setProperties(data.properties)});
+  }, []);
+
+  return (
+    <Fragment>
+      {properties.length > 0 && 
+        <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          src={properties[0].image_url}
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {properties[0].title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {properties[0].price_per_day} per night
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Details</Button>
+          <Button size="small">Book Now</Button>
+        </CardActions>
+        </Card>
+      }
+    </Fragment>
+  );
+}
