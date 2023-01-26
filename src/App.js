b41daac75d183dd6fc11e1098ba315193967c72b
imdodds <ianmitchelldodds@gmail.com>
2023-01-26 13:52:57 -0500

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Home from './components/Home'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   const [message, setMessage] = useState("");
@@ -16,6 +17,7 @@ function App() {
     <div className="app">
       <Header />
       <Home />
+      <Footer />
 
     {/* Home */}
       {/* Header */}
